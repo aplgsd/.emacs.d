c30d17ab9033bdcd2954796affb9dc19d2cd0f8a
tang.guangyue <aplgsdf@163.com>
2018-04-23 10:51:18 -0400

    開発関係の設定

---
+++ aplgsd/init-program.el
@@ -0,0 +1,22 @@
+;; Tool : git for use
+;;(global-set-key (kbd "C-x g") 'magit-status)
+;;(require 'evil-magit)
+;; Magit
+(setq-default magit-auto-revert-mode nil)
+(setq vc-handled-backends '())
+(eval-after-load "vc" '(remove-hook 'find-file-hooks 'vc-find-file-hook))
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-c l") 'magit-blame)
+
+(custom-set-faces
+ '(magit-diff-added ((t (:background "black" :foreground "green"))))
+ '(magit-diff-added-highlight ((t (:background "white" :foreground "green"))))
+ '(magit-diff-removed ((t (:background "black" :foreground "blue"))))
+ '(magit-diff-removed-hightlight ((t (:background "white" :foreground "blue"))))
+ '(magit-hash ((t (:foreground "red"))))
+)
+
+
+(provide 'init-program)
+
+
